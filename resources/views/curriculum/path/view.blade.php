b2a5d12d39fe7392b085cd616371781bfcab0833
mstaples <margaret.staples@gmail.com>
2020-04-29 10:08:15 +0000

    revert unintentional changes

--- resources/views/curriculum/path/view.blade.php
+++ resources/views/curriculum/path/view.blade.php
@@ -1,32 +1,34 @@
-
 @extends('layouts.left')
 
 @section('title', $title)
 
 @section('open-main')
     <div class="form-check">
-        {{ Form::open(array('url' => 'curriculum/questions/view/' . $pathId)) }}
-@endsection
+        {{ Form::open(array('url' => 'curriculum/prompts/view/' . $path->id)) }}
+        @endsection
 
-@section('sidebar')
-    <section class="box">
-        {{ Form::label('question_difficulty', 'question difficulty: '.ucfirst($question->question_difficulty)) }}
-        <br/>
-        {{ Form::submit('Continue to editors') }}
-    </section>
-@endsection
+        @section('sidebar')
+            <section class="box">
+                {{ Form::label('path_difficulty', 'Path difficulty: '.ucfirst($path->path_difficulty)) }}
+                <br/>
+                {{ Form::label('path_category', 'Path category: '.$path->category->name) }}
+                <br/>
+                {{ Form::label('existing_tags', 'Tags:') }}<br/>
+                @foreach ($path->getTags() as $tag)
+                    {{ Form::label('tag_'.$tag, $tag) }}<br/>
+                @endforeach
+                <br/>
+                {{ Form::submit('Continue to prompts') }}
+            </section>
+        @endsection
 
-@section('content')
-        {{ Form::label('question', 'question: '.$question->question) }}<br/>
-        {{ Form::label('answer_options', 'answer options:') }}<br/>
-        <ul>
-            @foreach($question->answer_options as $option => $correct)
-                <li>$option@if($correct) (correct)@endif</li>
-            @endforeach
-        </ul>
-@endsection
+        @section('content')
+            {{ Form::label('path_title', 'Path title: '.$path->path_title) }}<br/>
+            {{ Form::label('path_thesis', 'Path thesis:') }}<br/>
+            <p>{{ $path->path_thesis }}</p>
+        @endsection
 
-@section('close-main')
+        @section('close-main')
             {{ Form::close() }}
     </div>
 @endsection
